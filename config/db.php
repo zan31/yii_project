03a0297aa228060a5bd5b66e7cf7fc59d4857574
zan31 <zan.hrastnik31@gmail.com>
2023-10-11 13:42:25 +0200

    init commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=naloga',
+    'username' => 'zan31',
+    'password' => 'Skorpijon31',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
